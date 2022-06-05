c072736f90942754f12ca4d4ffbcc73e478c3c04
AnotherPillow <85362273+AnotherPillow@users.noreply.github.com>
2022-06-05 09:47:19 +0100

    Add another function - more in desc
    Credit to @GreenJuzzy for finding the new endpoints, which return JSON.

--- README.md
+++ README.md
@@ -1,18 +1,35 @@
-# skyblock.js
+skyblock.js
 A library for the skyblock.net api
+
 Currently only feature is to list online players.
 
-### Economy
+Economy
 ```js
 const sb = require("skyblock.js")
 sb.economy().then(data => {
     console.log(data);
 })
 ```
-### Survival
+Survival
 ```js
 const sb = require("skyblock.js")
 sb.survival().then(data => {
+    console.log(data);
+})
+```
+
+Classic
+```js
+const sb = require("skyblock.js")
+sb.classic().then(data => {
+    console.log(data);
+})
+```
+
+Friends
+```js
+const sb = require("skyblock.js")
+sb.friends("<FORUMS ID>").then(data => {
     console.log(data);
 })
 ```
--- index.js
+++ index.js
@@ -1,33 +1,66 @@
 const fetch = require("node-fetch");
 
+/*
+  Hello! Welcome to the Skyblock API!
+  Thanks to GreenJuzzy on Github for finding how to use some of the endpoints
+*/
+
+exports.classic = async function() {
+  const res = await fetch("https://skyblock.net/index.php?server-status/7/query", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+        "Accept": "application/json, text/javascript, */*; q=0.01",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "en-US,en;q=0.9,no;q=0.8",
+    },
+    body: "_xfRequestUri=%2F&_xfNoRedirect=1&_xfResponseType=json"
+  })
+  const json = await res.json()
+  return json;
+}
+
 exports.economy = async function() {
-    const res = await fetch("https://skyblock.net/?server-status");
-  const html = await res.text();
-  let SOR = html.indexOf("<body>Array")
-  let EOR = html.indexOf(`<div id="loginBar">`)
-  let body = html.substring(SOR, EOR).trim();
-  let p = [];
-  let biglongthing = body.substring(body.indexOf(`[4] => Array`), body.indexOf(`[6]`)).replace(`[4] => Array`, ``).replace("            ", "").replace("(", "").substring(body.indexOf(`[query_data] => a:4:`), body.indexOf(";}}")).split(`"`)
-  for (v in biglongthing) {
-    if (v % 2 == 1) {
-      p.push(biglongthing[v])
-    }
-  }
-  return p;
+  const res = await fetch("https://skyblock.net/index.php?server-status/6/query", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+        "Accept": "application/json, text/javascript, */*; q=0.01",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "en-US,en;q=0.9,no;q=0.8",
+    },
+    body: "_xfRequestUri=%2F&_xfNoRedirect=1&_xfResponseType=json"
+  })
+  const json = await res.json()
+  return json;
 }
 
 exports.survival = async function() {
-    const res = await fetch("https://skyblock.net/?server-status");
-    const html = await res.text();
-    let SOR = html.indexOf("<body>Array")
-    let EOR = html.indexOf(`<div id="loginBar">`)
-    let body = html.substring(SOR, EOR).trim();
-    let p = [];
-    let biglongthing = body.substring(body.indexOf(`[4] => Array`), body.indexOf(`[6]`)).replace(`[4] => Array`, ``).replace("            ", "").replace("(", "").substring(body.indexOf(`[query_data] => a:4:`), body.indexOf(";}}")).split(`"`)
-    for (v in biglongthing) {
-      if (v % 2 == 1) {
-        p.push(biglongthing[v])
-      }
-    }
-    return p;
-  }
+  const res = await fetch("https://skyblock.net/index.php?server-status/4/query", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+        "Accept": "application/json, text/javascript, */*; q=0.01",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "en-US,en;q=0.9,no;q=0.8",
+    },
+    body: "_xfRequestUri=%2F&_xfNoRedirect=1&_xfResponseType=json"
+  })
+  const json = await res.json()
+  return json;
+}
+
+exports.friends = async function(forums_id) {
+  const res = await fetch(`https://skyblock.net/friends-api/friends/get/${forums_id} `, {
+    method: "GET",
+    headers: {
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+        "Accept": "application/json, text/javascript, */*; q=0.01",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "en-US,en;q=0.9,no;q=0.8",
+    },
+  })
+  const json = await res.json()
+  return json;
+  
+}
