c79e46e8235af4245df4208bda80bf1670f08870
GreenJuzzy <65908209+GreenJuzzy@users.noreply.github.com>
2023-03-01 21:14:26 +0100

    Update index.d.ts
    Add new ServerList types

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-export type ServerList = "mineverse" | "skyblock" | "skywars"
+export type ServerList = "skyblock-bungee-1" | "skyblock-bungee-2" | "skyblock-bungee-3" | "skyblock-hub-1" | "skyblock-hub-2" | "skyblock-skyblock" | "skyblock-economy" | "skyblock-classic" | "skyblock" | "skyblock-events" | "skywars" | "mineverse"
 
 export type ServerCount = {
     result: number
