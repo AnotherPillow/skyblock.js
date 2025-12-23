3b081ed1801a9b8f600d6faca00235c746619130
AnotherPillow <85362273+AnotherPillow@users.noreply.github.com>
2025-12-23 19:54:07 +0000

    fix use of fetch() over wrapper

--- src/api/gamemode/info.ts
+++ src/api/gamemode/info.ts
@@ -1,8 +1,9 @@
 import { GameMode } from "../../types/remote";
 import { FullGamemodeInfoResponse } from "../../types/remote/gamemode";
+import fetcher from "../../util/fetcher";
 
 export async function getGamemode(gamemode: GameMode): Promise<FullGamemodeInfoResponse> {
-    const res = await fetch(`https://api.skyblock.net/gamemode/${gamemode}`)
+    const res = await fetcher(`https://api.skyblock.net/gamemode/${gamemode}`)
 
     return await res.json()
 }
