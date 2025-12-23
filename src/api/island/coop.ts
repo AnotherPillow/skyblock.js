c80de02a8d59c26509df965f0792ed97b846300d
AnotherPillow <85362273+AnotherPillow@users.noreply.github.com>
2025-12-23 19:10:21 +0000

    comment out functions that won't be available at first release

--- src/api/island/coop.ts
+++ src/api/island/coop.ts
@@ -2,8 +2,9 @@ import { GameMode } from "../../types/remote";
 import { FullIslandCoopResponse } from "../../types/remote/coop";
 import fetcher from "../../util/fetcher";
 
-export async function getCoop(gamemode: GameMode, uuid: string): Promise<FullIslandCoopResponse> {
-    const res = await fetcher(`https://api.skyblock.net/gamemode/${gamemode}/coop/${uuid}`)
+// not available in initial release
+// export async function getCoop(gamemode: GameMode, uuid: string): Promise<FullIslandCoopResponse> {
+//     const res = await fetcher(`https://api.skyblock.net/gamemode/${gamemode}/coop/${uuid}`)
 
-    return await res.json();
-}
+//     return await res.json();
+// }
--- src/api/island/island.ts
+++ src/api/island/island.ts
@@ -1,8 +1,9 @@
 import { FullIslandResponse, GameMode } from "../../types";
 import fetcher from "../../util/fetcher";
 
-export async function getIsland(gamemode: GameMode, uuid: string): Promise<FullIslandResponse> {
-    const res = await fetcher(`https://api.skyblock.net/gamemode/${gamemode}/island/${uuid}`)
+// not available in initial release
+// export async function getIsland(gamemode: GameMode, uuid: string): Promise<FullIslandResponse> {
+//     const res = await fetcher(`https://api.skyblock.net/gamemode/${gamemode}/island/${uuid}`)
 
-    return await res.json();
-}
+//     return await res.json();
+// }
